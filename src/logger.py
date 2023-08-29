114aec7d823c08db2586162fbe1a56266236e88a
Ankit_zanzmera <zanzmeraankit0@gmaul.com>
2023-08-29 18:39:05 +0530

    logger file complete

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import os
+import logging
+from datetime import datetime
+
+Dir_name = f"{datetime.now().strftime('%d_%m_%Y')}"
+file_name = f"{datetime.now().strftime('time_%H_%M_%S.log')}"
+
+DIR_PATH = os.path.join(os.getcwd(),"logs",Dir_name)
+os.makedirs(DIR_PATH,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(DIR_PATH,file_name)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format = "[ %(asctime)s ] %(lineno)d - %(name)s - %(levelname)s - %(message)s ",
+)
+
+logging.info('logger file created sucessfully')
+
